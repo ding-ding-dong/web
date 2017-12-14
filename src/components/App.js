cebc48dc2d8a0e18501c22da7ff0e9a469b2d946
Mystist <gjl87910lq@gmail.com>
2017-12-14 16:05:39 +0800

    chore: material-ui demos

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
 
+import PersistentDrawer from './demos/drawers/PersistentDrawer'
+
 class App extends Component {
   render() {
     return (
-      <div>
-        web
-      </div>
+      <PersistentDrawer />
     )
   }
 }
